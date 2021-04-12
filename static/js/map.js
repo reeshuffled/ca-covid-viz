5067323852915d97b81070ff1a4fe6b2caf9fa55
Rees Draminski <rees.draminski@gmail.com>
2021-04-12 14:57:09 -0400

    Clean up map code

--- static/js/map.js
+++ static/js/map.js
@@ -3,13 +3,18 @@ const map = L.map('map').setView([37.8, -96], 4);
 // control that shows state info on hover
 const info = L.control();
 
-// global variable for cases
+const dateInput = document.getElementById("dateInput");
+
+// global variables
+let geojson;
 let cases = [];
+const counties = [];
 
 /**
  * Initialize the UI components.
  */
 (async function initUI() {
+    // add mapbox map data
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -18,23 +23,18 @@ let cases = [];
         tileSize: 512,
         zoomOffset: -1
     }).addTo(map);
-    
+     
+    // add the cases info box element
+    addInfoBox();
+
+    // generate legend and add to map
+    addMapLegend();
+
+    // add data attribution for map
+    map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
     // bind date input to get case data
-    document.getElementById("dateInput").oninput = e => getCasesByDate(e.target.value);
-
-    info.onAdd = function (map) {
-        this._div = L.DomUtil.create('div', 'info');
-        this.update();
-        return this._div;
-    };
-
-    info.update = function (props) {
-        this._div.innerHTML = '<h4>US Cases</h4>' +  (props ?
-            '<b>' + props.name + '</b><br />' + props.cases + ' cases<br /> ' + props.deaths + ' deaths<br />' : 'Hover over a county');
-            //the stuff that needs are placeholders are props.cases and deaths. They need to take data from the database
-    };
-
-    info.addTo(map);
+    dateInput.oninput = e => getCasesByDate(e.target.value);
     
     // get the case data for today
     const today = new Date();
@@ -42,13 +42,143 @@ let cases = [];
 
     // load the state GeoJSON data
     geojson = L.geoJson(statesData, {
-        style: style,
-        onEachFeature: onEachFeature
+        style: getCountyStyle,
+        onEachFeature: (feature, layer) => {
+            // bind mouseo and click events for highlighting and zooming
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+
+            // if the feature is a county
+            if (layer.feature.properties.kind == "county")
+            {
+                // add the feature to the counties array
+                counties.push({
+                    layer: layer,
+                    feature: layer.feature
+                });
+
+                // find the case data for the county
+                const caseData = cases.find(x => x.county == layer.feature.properties.name);
+
+                // if there is case data found for the county
+                if (caseData != null)
+                {
+                    // update the county cases and death properties
+                    layer.feature.properties.cases = caseData.cases;
+                    layer.feature.properties.deaths = caseData.deaths;
+
+                    // update the style because the case data changed
+                    layer.setStyle(getCountyStyle(feature));
+                }
+            }
+        }
     }).addTo(map);
 })();
 
-// get color depending on population cases value
-function getCases(d) {
+/**
+ * Add the cases info box element.
+ */
+function addInfoBox() {
+    info.onAdd = function(map) {
+        this._div = L.DomUtil.create('div', 'info');
+
+        this.update();
+
+        return this._div;
+    };
+
+    info.update = function(props) {
+        this._div.innerHTML = '<h4>US Cases</h4>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.cases + ' cases<br /> ' + props.deaths + ' deaths<br />' : 'Hover over a county');
+    };
+
+    info.addTo(map);
+}
+
+/**
+ * A function that queries the API for case data for a specific date.
+ * @param {String} date
+ */
+async function getCasesByDate(date) {
+    // make a POST request to the /date endpoint of the Flask server
+    const request = await fetch("/date", {
+        method: "POST",
+        // let the server know we're POSTing JSON data
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        // stringify the JSON for transport
+        body: JSON.stringify({
+            date: date
+        })
+    });
+
+    // get the response JSON data from the server
+    const response = await request.json();
+    const data = response.data;
+
+    // update cases global variable with fetched data
+    cases = data;
+
+    /**
+     * set the date input to the date in the JSON response, this is necessary in
+     * case of date rollbacks because we don't have data for that date
+     */
+    dateInput.value = response.date;
+
+    // update the county coloring by case data
+    counties.forEach(x => x.layer.setStyle(getCountyStyle(x.feature)));
+}
+
+/**
+ * Create map legend for the choropleth colors for case numbers and add it to the
+ * map.
+ */
+function addMapLegend() {
+    const legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [
+                0,
+                10000,
+                15000,
+                20000,
+                40000,
+                60000,
+                80000,
+                1000000
+            ],
+            labels = [],
+            from, to;
+
+        for (var i = 0; i < grades.length; i++) {
+            from = grades[i];
+            to = grades[i + 1];
+
+            labels.push(
+                '<i style="background:' + getFillColorByCases(from + 1) + '"></i> ' +
+                from + (to ? '&ndash;' + to : '+'));
+        }
+
+        div.innerHTML = labels.join('<br>');
+
+        return div;
+    };
+
+    legend.addTo(map);
+}
+
+/**
+ * Get the corresponding color for a daily case number within a range.
+ * @param {Number} d 
+ * @returns {String} color
+ */
+function getFillColorByCases(d) {
     return d > 1000000 ? '#800026' :
             d > 80000  ? '#BD0026' :
             d > 60000  ? '#E31A1C' :
@@ -59,49 +189,28 @@ function getCases(d) {
                         '#FFEDA0';
 }
 
-function style(feature) {
+/**
+ * Return a feature style for a county that is colored by case data range.
+ * @param {Feature} feature 
+ * @returns {Style} style
+ */
+function getCountyStyle(feature) {
     return {
         weight: 2,
         opacity: 1,
         color: 'white',
         dashArray: '3',
         fillOpacity: 0.7,
-        fillColor: getCases(feature.properties.cases)//this is the a placeholder getCases needs to get the amount of cases from a database
+        fillColor: getFillColorByCases(feature.properties.cases)//this is the a placeholder getCases needs to get the amount of cases from a database
     };
 }
 
-
-
 /**
- * A function that queries the API for case data for a specific date.
- * @param {String} date
+ * Highlight a feature on hover.
+ * @param {Event} e 
  */
-async function getCasesByDate(date) {
-    // make a POST request to the /date endpoint of the Flask server
-    const request = await fetch("/date", { 
-        method: "POST",
-        // let the server know we're POSTing JSON data
-        headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-        },
-        // stringify the JSON for transport
-        body: JSON.stringify({ 
-            date: date
-        })
-    });
-
-    // get the response JSON data from the server
-    const response = await request.json();
-    const data = response.data;
-
-    cases = data;
-
-    document.getElementById("dateInput").value = response.date;
-}
-
 function highlightFeature(e) {
-    var layer = e.target;
+    const layer = e.target;
 
     layer.setStyle({
         weight: 5,
@@ -124,70 +233,12 @@ function highlightFeature(e) {
     info.update(layer.feature.properties);
 }
 
-var geojson;
-
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
+
     info.update();
 }
 
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-
-    if (layer.feature.properties.kind == "county")
-    {
-        const caseData = cases.find(x => x.county == layer.feature.properties.name);
-
-        if (caseData != null)
-        {
-            layer.feature.properties.cases = caseData.cases;
-            layer.feature.properties.deaths = caseData.deaths;
-
-            layer.setStyle(style(feature));
-        }
-    }
-}
-
-map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
-
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [
-            0,
-            10000,
-            15000,
-            20000,
-            40000,
-            60000,
-            80000,
-            1000000
-        ],
-        labels = [],
-        from, to;
-
-    for (var i = 0; i < grades.length; i++) {
-        from = grades[i];
-        to = grades[i + 1];
-
-        labels.push(
-            '<i style="background:' + getCases(from + 1) + '"></i> ' +
-            from + (to ? '&ndash;' + to : '+'));
-    }
-
-    div.innerHTML = labels.join('<br>');
-    return div;
-};
-
-legend.addTo(map);
