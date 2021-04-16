68313c9e3dd73530688564352d5c49ae4c96ac1d
jfluhar1 <70669287+jfluhar1@users.noreply.github.com>
2021-04-16 16:09:44 -0400

    points now display on map much like the GEOJSON's

--- static/js/map.js
+++ static/js/map.js
@@ -85,27 +85,70 @@ const prisons = [];
         // get latitude and longitude coordinates
         const [ lat, lng ] = feature.geometry.coordinates;
         
-        // add marker for location of prison
-        var prison = L.circle([lat, lng], {
-            color: 'white',
-            fillColor: '#FFFFFF',
-            fillOpacity: 1,
-            radius: 100,
-
-        }).addTo(map);
-
         // add radius around prison
         var circle = L.circle([lat, lng], {
+            color: 'black',
             fillColor: '#000000',
             fillOpacity: .4,
             radius: 1000
         }).addTo(map);
 
-        circle.layer.on({
+        // add marker for location of prison
+        var prison = L.circle([lat, lng], {
 
-            mouseover: highlightFeature
+            onEachFeature: (feature, layer) => {
 
-        });
+                layer.on({
+
+                    //mouseover: highlightFeature
+                    onclick: ("Fucks")
+                });
+
+                if (layer.feature.properties.kind == "prison"){
+                    // add the feature to the counties array
+                    prisons.push({
+                        layer: layer,
+                        feature: layer.feature
+                    });
+
+                    // find the case data for the county
+                    const prsnCaseData = cases.prison.find(x => x.prison == layer.feature.properties.name);
+
+                    // if there is case data found for the county
+                    if (prsnCaseData != null){
+                        // update the county cases and death properties
+                        layer.feature.properties.date = prsnCaseData.date;
+                        layer.feature.properties.casesRes = prsnCaseData.residentsConfirmed;
+                        layer.feature.properties.casesStaff = prsnCaseData.staffConfirmed;
+
+                        layer.feature.properties.deathsRes = prsnCaseData.residentsDeaths;
+                        layer.feature.properties.deathsStaff = prsnCaseData.staffDeaths;
+
+                        layer.feature.properties.residentsRecovered = prsnCaseData.residentsRecovered;
+                        layer.feature.properties.staffRecovered = prsnCaseData.staffRecovered;
+
+                        layer.feature.properties.popFebTwenty = prsnCaseData.popFebTwenty;
+                        layer.feature.properties.residentsPopulation = prsnCaseData.residentsPopulation;
+
+                        layer.feature.properties.county = prsnCaseData.county;
+                        layer.feature.properties.staffRecovered = prsnCaseData.staffRecovered;
+
+                        lat = prsnCaseData.latitude;
+                        lng = prsnCaseData.longitude;
+
+                        // update the style because the case data changed
+                        //layer.setStyle(getCountyStyle(feature));
+                    }
+                }
+
+            },
+
+            color: 'black',
+            fillColor: '#000000',
+            fillOpacity: 1,
+            radius: 100,
+
+        }).addTo(map);
 
     })
 })();
